cd4a99cee21f70ad0e1b2609e9b28972640d70a5
ravine13 <kuriaderek@gmail.com>
2023-12-02 09:14:03 +0300

    Object Relationship Methods

--- lib/customers.py
+++ lib/customers.py
@@ -1,9 +1,13 @@
+
+from Review import Review
+
 class Customer:
     all_customers = []
 
     def __init__(self, given_name, family_name):
         self._given_name = given_name
         self._family_name = family_name
+        self.reviews = []
         Customer.all_customers.append(self)
     
     @property
@@ -20,3 +24,12 @@ class Customer:
     @classmethod
     def all(cls):
         return cls.all_customers
+    
+    def restaurants(self):
+        return list(set(review.restaurant for review in self.reviews))
+    
+    def add_review(self,restaurant,review):
+        new_review = Review(self,restaurant,review)
+        self.reviews.append(new_review)
+        
+
